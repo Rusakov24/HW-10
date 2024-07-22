a9a35e607e3f45b98fa354bbd9d5eb1291e54a93
Rusakov24 <lovewillkillme@vk.com>
2024-07-22 17:12:31 +0300

    Начало работы

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("\nЗадание 1");
+
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+
+        String fullName = lastName + " " + firstName + " " + middleName;
+
+        System.out.println("Ф. И. О. сотрудника — " + fullName);
+
+        System.out.println("\nЗадание 2");
+        String small = fullName;
+
+        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета — " + small.toUpperCase());
+
+        System.out.println("\nЗадание 3");
+            }
+        }
+
